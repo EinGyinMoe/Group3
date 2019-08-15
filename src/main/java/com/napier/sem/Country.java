fb6eb3ab727e4a7d5637cf3db0622149511de448
KhinShoon <40414299@napier.ac.uk>
2019-08-15 17:07:20 +0630

    city in a continent by population function

---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,54 @@
+package com.napier.sem;
+
+/**
+ * Represents an country
+ */
+public class Country {
+    /**
+     * Code
+     */
+    public String Code;
+
+    /**
+     * Country's Name
+     */
+    public String Name;
+
+    /**
+     * Country's Continent
+     */
+    public String Continent;
+
+    /**
+     * Region
+     */
+    public String Region;
+
+    /**
+     * City's surface area
+     */
+    public float SurfaceArea;
+
+    /**
+     * City's IndepYear
+     */
+    public int IndepYear;
+
+    public int Population;
+
+    public float LifeExpectancy;
+
+    public float GNP;
+
+    public float GNPOld;
+
+    public String LocalName;
+
+    public String GovernmentForm;
+
+    public String HeadOfState;
+
+    public int Capital;
+
+    public String Code2;
+}
