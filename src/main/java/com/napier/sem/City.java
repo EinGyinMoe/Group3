9fb607d70f16af5be2afd24e26bb890b00a4c95f
EinGyinMoe <40414316@napier.ac.uk>
2019-08-21 15:59:27 +0630

    Changes public to private

--- src/main/java/com/napier/sem/City.java
+++ src/main/java/com/napier/sem/City.java
@@ -1,3 +1,6 @@
+
+//CITY.java
+
 package com.napier.sem;
 
 /**
@@ -5,28 +8,54 @@ package com.napier.sem;
  */
 public class City
 {
-    /**
-     * City ID
-     */
-    public int ID;
+    private int ID;
 
-    /**
-     * City's Name
-     */
-    public String Name;
+    public int getID() {
+        return ID;
+    }
 
-    /**
-     * Country's code
-     */
-    public String CountryCode;
+    public void setID(int ID) {
+        this.ID = ID;
+    }
 
-    /**
-     * District
-     */
-    public String District;
+    private String Name;
 
-    /**
-     * City's population
-     */
-    public int Population;
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    private String CountryCode;
+
+    public String getCountryCode() {
+        return CountryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        CountryCode = countryCode;
+    }
+
+    private String District;
+
+    public String getDistrict() {
+        return District;
+    }
+
+    public void setDistrict(String district) {
+        District = district;
+    }
+
+    private int Population;
+
+    public int getPopulation() {
+        return Population;
+    }
+
+    public void setPopulation(int population) {
+        Population = population;
+    }
+
 }
