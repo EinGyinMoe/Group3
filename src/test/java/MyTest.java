8965e22815c0e8d4bde19b39597d6e1d03f4a9a9
EinGyinMoe <40414316@napier.ac.uk>
2019-08-23 11:09:19 +0630

    Added country and report in unit testing

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -8,25 +8,37 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 public class MyTest
 {
-    static App app;
 
+    static App app;
     @BeforeAll
     static void init()
     {
         app = new App();
+        {
+            System.out.println("\n...........................\n" +
+                    "Unit_Testing For WorldFact\n" +
+                    "...........................");
+        }
+    }
+
+    //Unit_testing for CityDistrict
+    @Test
+    protected void  displayCityDistricttest()
+    {
+        app.displayCityDistrict(null);
     }
 
     @Test
-    protected void displayCityDistrict()
-    {
-        ArrayList<City> cty = new ArrayList<City>();
-        cty.add(null);
-        app.displayCityDistrict(cty);
+    protected void  displayCityDistrictNulltest()
+    {   ArrayList<City> cities =new ArrayList<>();
+        cities.add(null);
+        app.displayCityDistrict(cities);
     }
 
-
+    //Unit_testing for CityContinent
     @Test
     protected void  displayCityContinenttest()
     {
@@ -34,12 +46,14 @@ public class MyTest
     }
 
     @Test
-    protected void  displayCityNulltest()
+    protected void  displayCityContinentNulltest()
     {   ArrayList<City> cities =new ArrayList<>();
         cities.add(null);
         app.displayCityContinent(cities);
     }
 
+
+    //Unit_testing for Cityreport
     @Test
     protected void  displayCityReporttest()
     {
@@ -48,24 +62,40 @@ public class MyTest
 
     @Test
     protected void  displayCityReportNulltest()
-    {   ArrayList<City> cit =new ArrayList<>();
-        cit.add(null);
-        app.displayCityReport(cit);
+    {   ArrayList<City> cities =new ArrayList<>();
+        cities.add(null);
+        app.displayCityReport(cities);
     }
 
+    //Unit_testing for CityRegion
     @Test
-    protected void  displayCityRegionttest()
+    protected void  displayCityRegiontest()
     {
         app.displayCityRegion(null);
     }
 
     @Test
     protected void  displayCityRegionNulltest()
-    {   ArrayList<City> ct =new ArrayList<>();
-        ct.add(null);
-        app.displayCityRegion(ct);
+    {   ArrayList<City> cities =new ArrayList<>();
+        cities.add(null);
+        app.displayCityRegion(cities);
+    }
+    //Unit_testing for CityWorld
+    @Test
+    protected void  displayCityWorldtest()
+    {
+        app.displayCityWorld(null);
     }
 
+    @Test
+    protected void  displayCityWorldNulltest()
+    {   ArrayList<City> cities =new ArrayList<>();
+        cities.add(null);
+        app.displayCityWorld(cities);
+    }
+
+
+    //Unit_testing for CityCountry
     @Test
     protected void  displayCityCountrytest()
     {
@@ -74,9 +104,53 @@ public class MyTest
 
     @Test
     protected void  displayCityCountryNulltest()
-    {   ArrayList<City> cty =new ArrayList<>();
-        cty.add(null);
-        app.displayCityCountry(cty);
+    {   ArrayList<City> cities =new ArrayList<>();
+        cities.add(null);
+        app.displayCityCountry(cities);
     }
+
+    //Unit_testing for CountryContinent
+    @Test
+    protected void  displayCountryContinenttest()
+    {
+        app.displayCountryContinent(null);
+    }
+
+    @Test
+    protected void  displayCountryContinentNulltest()
+    {   ArrayList<Country> countries =new ArrayList<>();
+        countries.add(null);
+        app.displayCountryContinent(countries);
+    }
+
+    //Unit_testing for CountryRegion
+    @Test
+    protected void  displayCountryRegiontest()
+    {
+        app.displayCountryContinent(null);
+    }
+
+    @Test
+    protected void  displayCountryRegionNulltest()
+    {   ArrayList<Country> countries =new ArrayList<>();
+        countries.add(null);
+        app.displayCountryRegion(countries);
+    }
+
+    //Unit_testing for CountryWorld
+    @Test
+    protected void  displayCountryWorldtest()
+    {
+        app.displayCountryWorld(null);
+    }
+
+    @Test
+    protected void  displayCountryWorldNulltest()
+    {   ArrayList<Country> countries =new ArrayList<>();
+        countries.add(null);
+        app.displayCountryWorld(countries);
+    }
+
+
 
 }
