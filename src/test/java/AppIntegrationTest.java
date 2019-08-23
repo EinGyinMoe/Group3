93a623ea1b340a8913e6d308c63f450670ecbf09
KhinShoon <40414299@napier.ac.uk>
2019-08-23 16:01:54 +0630

    Integration Test 3

--- src/test/java/AppIntegrationTest.java
+++ src/test/java/AppIntegrationTest.java
@@ -35,7 +35,7 @@ public class AppIntegrationTest
     }
 
     @Test
-    void testgetCountryByCode2() throws SQLException
+    void testgetCountryContinent() throws SQLException
     {
         ArrayList<Country> ctries= app.getCountryContinent();
         for (Country c: ctries) {
@@ -46,4 +46,63 @@ public class AppIntegrationTest
         }
     }
 
+    @Test
+    void testgetCityDistrict() throws SQLException
+    {
+        ArrayList<City> cities= app.getCityDistrict();
+        for (City c: cities) {
+            if (c.getName().equals("Kabul")) {
+                assertEquals(c.getDistrict(), "Kabol");
+                assertEquals(c.getPopulation(), 1780000);
+            }
+        }
+    }
+
+    //2. All the country in a continent by Population
+    @Test
+    void testgetCityContinent() throws SQLException
+    {
+        ArrayList<City> cities= app.getCityContinent();
+        for (City c: cities) {
+            if (c.getName().equals("Dubai")) {
+                assertEquals(c.getCountry().getContinent(), "Asia");
+                assertEquals(c.getPopulation(), 669181);
+            }
+        }
+    }
+
+    //3. city report requires the following columns:
+//Name.
+//Country.
+//District.
+//Population.
+//
+//    @Test
+//    void testgetCity_report() throws SQLException
+//    {
+//        ArrayList<City> cities= app.getCity_report();
+//        for (City c: cities) {
+//            if (c.getName().equals("Dubai")) {
+//                assertEquals(c.getCountry().getName(), "United Arab Emirates");
+//                assertEquals(c.getDistrict(), "Dubai");
+//                assertEquals(c.getPopulation(), 669181);
+//            }
+//        }
+//    }
+
+
+    //7.
+//all the countries in a region by the population
+//
+    @Test
+    void testgetCountryRegion() throws SQLException
+    {
+        ArrayList<Country> ctries= app.getCountryRegion();
+        for (Country c: ctries) {
+            if (c.getName().equals("Canada")) {
+                assertEquals(c.getCode(), "CAN");
+                assertEquals(c.getPopulation(), 31147000);
+            }
+        }
+    }
 }
